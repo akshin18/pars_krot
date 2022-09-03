fc40668a6b83d8fc1b1ae16859781bdb26dd76cb
akshin18 <akshinyolcuyev17@gmail.com>
2022-09-03 16:01:07 +0400

    uper

--- cokkies.py
+++ cokkies.py
@@ -39,7 +39,7 @@ def get_pages(page,txt,date_s,date_e):
     "cookie=__ddg1_=8TdVxNSiol0DUc8R1Gsp; PHPSESSID=ok5nufm4vcphhmek2v0ud0n2b1; tpABt=1; _gcl_au=1.1.803266544.1661407846; _ym_d=1661407852; _ym_uid=1661365888112088091; _gid=GA1.2.2109736412.1662205198; _ym_visorc=w; _ym_isad=2; _ga=GA1.1.304858025.1661407845; _ga_2YYFBYZ073=GS1.1.1662205198.25.1.1662206069.0.0.0")
     # driver = webdriver.Chrome(chrome_options=chrome_options)
     driver = webdriver.Chrome(ChromeDriverManager().install(),chrome_options=chrome_options)
-    driver.get(f"https://tbankrot.ru/reestr?page={page}&type=mess&text={txt}&debtor=&dt_1={date_s}&dt_2={date_e}&t%5B%5D=2&t%5B%5D=7")
+    driver.get(f"http://82.202.170.158/reestr?page={page}&type=mess&text={txt}&debtor=&dt_1={date_s}&dt_2={date_e}&t%5B%5D=2&t%5B%5D=7")
     res = driver.page_source.encode("utf-8")
     driver.close()
     return res
