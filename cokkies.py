89bd2ee799cf4dfeb0ef75769f8cc6852c9b272a
akshin18 <akshinyolcuyev17@gmail.com>
2022-09-03 15:57:28 +0400

    uper

--- cokkies.py
+++ cokkies.py
@@ -35,6 +35,8 @@ def get_pages(page,txt,date_s,date_e):
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument(
     "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
+    chrome_options.add_argument(
+    "cookie=__ddg1_=8TdVxNSiol0DUc8R1Gsp; PHPSESSID=ok5nufm4vcphhmek2v0ud0n2b1; tpABt=1; _gcl_au=1.1.803266544.1661407846; _ym_d=1661407852; _ym_uid=1661365888112088091; _gid=GA1.2.2109736412.1662205198; _ym_visorc=w; _ym_isad=2; _ga=GA1.1.304858025.1661407845; _ga_2YYFBYZ073=GS1.1.1662205198.25.1.1662206069.0.0.0")
     # driver = webdriver.Chrome(chrome_options=chrome_options)
     driver = webdriver.Chrome(ChromeDriverManager().install(),chrome_options=chrome_options)
     driver.get(f"https://tbankrot.ru/reestr?page={page}&type=mess&text={txt}&debtor=&dt_1={date_s}&dt_2={date_e}&t%5B%5D=2&t%5B%5D=7")
